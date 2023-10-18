9f9da791e828681c00a21da00ac6020a50daaffa
Jordan <46637683+JordanllHarper@users.noreply.github.com>
2023-10-18 22:34:36 +0100

    Remove unnecessary post (for now)

--- src/endpoints/notes/handlers.rs
+++ src/endpoints/notes/handlers.rs
@@ -3,16 +3,11 @@ use rocket::{response::status::NotFound, serde::json::Json, Route};
 use super::dto::GetNotesResponseDto;
 
 pub fn provide_routes_notes() -> Vec<Route> {
-    routes![get_index, post_index]
+    routes![get_notes]
 }
 
-//TODO: Change this to actual server response in working build
+//TODO: Change this to actual notes response in working build
 #[get("/notes")]
-pub fn get_index() -> Result<Json<GetNotesResponseDto>, NotFound<String>> {
+pub fn get_notes() -> Result<Json<GetNotesResponseDto>, NotFound<String>> {
     Ok(Json(GetNotesResponseDto::dummy()))
-}
-
-#[post("/notes", format = "application/json", data = "<note>")]
-pub fn post_index(note: rocket::serde::json::Json<crate::models::note::Note>) -> String {
-    String::from("Sucessful post")
 }
