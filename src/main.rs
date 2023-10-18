158f70591b1ab0bc0497b31861a55a4f599383f4
Jordan <46637683+JordanllHarper@users.noreply.github.com>
2023-10-18 20:43:17 +0100

    Getting started with rocket stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+fn get_index() -> String {
+    "Hello, World!".to_string()
+}
+
+#[get("/greeting/<name>")]
+fn get_name(name: &str) -> String {
+    format!("Hello, {}", name)
+}
+
+#[post("/")]
+fn post_index() -> String {
+    String::from("Sucessful post")
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![get_index, post_index])
+        .mount("/greeting", routes![get_name])
 }
