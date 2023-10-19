7b933978f002e9145d305c81303b129166ad0139
Jordan <46637683+JordanllHarper@users.noreply.github.com>
2023-10-19 23:11:52 +0100

    We be starting from scratch with ftp

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,3 @@
-#[macro_use]
-extern crate rocket;
-mod endpoints;
-mod models;
-
-#[launch]
-fn rocket() -> _ {
-    let notes_endpoint = "/notes";
-
-    let notes_routes = endpoints::notes::handlers::provide_routes_notes();
-
-    rocket::build().mount(notes_endpoint, notes_routes)
+fn main() {
+    println!("Hello, World!");
 }
