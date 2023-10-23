790cd5c006ecb35cc56060071737ddc038d2033b
JordanH <jordanh@apadmi.com>
2023-10-23 19:21:17 +0100

    Got the server running

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, World!");
+use unftp_sbe_fs::ServerExt;
+
+#[tokio::main]
+pub async fn main() {
+    // Startup server
+    let ftp_home = std::env::temp_dir();
+    let server = libunftp::Server::with_fs(ftp_home)
+        .greeting("Welcome to my ftp server.")
+        .passive_ports(50000..65535);
+
+
+
+    let ip = "127.0.0.1:2121";
+    // Listen...
+    println!("Server listening on ftp://{}", ip);
+    let _= server.listen(ip).await;
 }
