bc00f675b1472f5bc5bdd6a8baea5d3b2df12a88
JordanH <jordanh@apadmi.com>
2023-10-23 22:34:26 +0100

    Read credentials from file system

--- notes-client/src/main.rs
+++ notes-client/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use std::{fmt::Display, fs};
+
+use anyhow::Ok;
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+struct Credentials {
+    username: String,
+    password: String,
+}
+
+fn main() -> anyhow::Result<()> {
+    let creds = get_credentials()?;
+    println!("{:#?}", creds);
+    Ok(())
+}
+
+fn get_credentials() -> anyhow::Result<Credentials> {
+    let file_contents = fs::read_to_string("credentials.json")
+        .expect("Put credentials in a provided credentials.json file.");
+    let creds: Credentials = serde_json::from_str(&file_contents.to_string())?;
+    Ok(creds)
 }
