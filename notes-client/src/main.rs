3becda349b2e9ef648a37ce244c238ef0dba9e91
JordanH <jordanh@apadmi.com>
2023-10-23 22:36:25 +0100

    Add ftp client

--- notes-client/src/main.rs
+++ notes-client/src/main.rs
@@ -8,16 +8,15 @@ struct Credentials {
     username: String,
     password: String,
 }
-
-fn main() -> anyhow::Result<()> {
-    let creds = get_credentials()?;
-    println!("{:#?}", creds);
-    Ok(())
-}
-
 fn get_credentials() -> anyhow::Result<Credentials> {
     let file_contents = fs::read_to_string("credentials.json")
         .expect("Put credentials in a provided credentials.json file.");
     let creds: Credentials = serde_json::from_str(&file_contents.to_string())?;
     Ok(creds)
+}
+
+fn main() -> anyhow::Result<()> {
+    let creds = get_credentials()?;
+
+    Ok(())
 }
