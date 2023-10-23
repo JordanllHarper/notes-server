6651569925c8a52b2bb01f28e445ff344492a106
JordanH <jordanh@apadmi.com>
2023-10-23 20:59:58 +0100

    Change fs to use temp_dir

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,21 @@
+use std::sync::Arc;
+
 use unftp_auth_jsonfile::JsonFileAuthenticator;
+use unftp_sbe_fs::ServerExt;
 
 #[tokio::main]
-pub async fn main() -> Result<(), Box<dyn std::error::Error>>{
-
+pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
     //Set up authentication
     let authenticator = JsonFileAuthenticator::from_file(String::from("credentials.json"))?;
     // Startup server
-    let server = libunftp::Server::with_authenticator(Box::new(move || {unftp_sbe_fs::Filesystem::new("/srv/ftp")}),
-    std::sync::Arc::new(authenticator)
-    )
+    let server = libunftp::Server::with_fs(std::env::temp_dir())
+        .authenticator(Arc::new(authenticator))
         .greeting("Welcome to my ftp server.")
         .passive_ports(50000..65535);
-
-
 
     let ip = "127.0.0.1:2121";
     // Listen...
     println!("Server listening on ftp://{}", ip);
-    let _= server.listen(ip).await;
+    let _ = server.listen(ip).await;
     Ok(())
 }
