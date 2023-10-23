5009cb2049e247032f6b8678f44a29f6a4456417
JordanH <jordanh@apadmi.com>
2023-10-23 21:41:32 +0100

    Use runtime instead of async tokio main

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,28 @@
-use std::sync::Arc;
+use std::{error::Error, sync::Arc};
 
 use unftp_auth_jsonfile::JsonFileAuthenticator;
 use unftp_sbe_fs::ServerExt;
 
-#[tokio::main]
-pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
+pub fn main() -> Result<(), Box<dyn Error>> {
     //Set up authentication
     let authenticator = JsonFileAuthenticator::from_file(String::from("credentials.json"))?;
+    let ftp_home = std::env::temp_dir();
     // Startup server
-    let server = libunftp::Server::with_fs(std::env::temp_dir())
+    let server = libunftp::Server::with_fs(ftp_home)
         .authenticator(Arc::new(authenticator))
         .greeting("Welcome to my ftp server.")
         .passive_ports(50000..65535);
 
     let ip = "127.0.0.1:2121";
     // Listen...
+
+    let runtime = tokio::runtime::Builder::new_current_thread()
+        .enable_io()
+        .enable_time()
+        .build()
+        .unwrap();
+
     println!("Server listening on ftp://{}", ip);
-    let _ = server.listen(ip).await;
+    runtime.block_on(server.listen(ip))?;
     Ok(())
 }
