b31e6473e4dd5247186eee894b698395c546199d
JordanH <jordanh@apadmi.com>
2023-10-31 23:36:46 +0000

    Some further refinements and organisation

--- notes-client/src/md_interactor.rs
+++ notes-client/src/md_interactor.rs
@@ -1,32 +1,39 @@
-use anyhow::Result;
+use anyhow::{Ok, Result};
 use ftp::{FtpError, FtpStream};
 
 pub fn get_current_dir(ftp_stream: &mut FtpStream) -> anyhow::Result<String, FtpError> {
     match ftp_stream.pwd() {
-        Ok(v) => Result::Ok(v),
+        std::result::Result::Ok(v) => Result::Ok(v),
         Err(e) => Err(e),
     }
 }
 pub fn list_current_dir(ftp_stream: &mut FtpStream) -> anyhow::Result<Vec<String>, FtpError> {
     match ftp_stream.list(None) {
-        Ok(v) => Result::Ok(v),
+        std::result::Result::Ok(v) => Result::Ok(v),
         Err(e) => Err(e),
     }
 }
 pub fn cd_md_dir(ftp_stream: &mut FtpStream, md_path_in_ftp: &str) -> anyhow::Result<(), FtpError> {
     match ftp_stream.cwd(md_path_in_ftp) {
-        Ok(v) => Result::Ok(v),
+        std::result::Result::Ok(v) => Result::Ok(v),
         Err(e) => Err(e),
     }
 }
 
 pub fn make_md_notes(ftp_stream: &mut FtpStream, filename: &str) -> anyhow::Result<(), FtpError> {
     match ftp_stream.mkdir(&format!("{}{}", "./", filename)) {
-        Ok(v) => Result::Ok(v),
+        std::result::Result::Ok(v) => Result::Ok(v),
         Err(e) => Err(e),
     }
 }
 
-fn store_markdown_docs() {
-    todo!()
+pub fn pull_docs(option: Option<String>, root_filename: String) -> anyhow::Result<()> {
+    println!("Pull docs ran");
+
+    Ok(())
+}
+
+pub fn push_docs(option: Option<String>, root_filename: String) -> anyhow::Result<()> {
+    println!("Push docs ran");
+    Ok(())
 }
